6961d0cf9833fec611a193d3d2276c662aabea4e
namoo0515 <eunjikim0211@gmail.com>
2026-07-02 13:48:56 +0900

    feat: cicd 테스트1

--- backend/src/main/java/com/nowait/domain/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/nowait/domain/auth/dto/LoginRequest.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
 
-    @NotBlank(message = "이메일을 입력해주세요!") @Email(message = "올바른 이메일 형식이 아닙니다.") String email,
+        @NotBlank(message = "이메일을 입력해주세요!") @Email(message = "올바른 이메일 형식이 아닙니다.") String email,
 
-    @NotBlank(message = "비밀번호를 입력해주세요!") String password) {
+        @NotBlank(message = "비밀번호를 입력해주세요!") String password) {
 }
+
+// CICD 테스트용 주석
